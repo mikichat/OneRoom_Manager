90218acfedfaa2a71801e98f1e11d419b50f8737
mikichat <mikichat@gmail.com>
2025-07-10 22:57:40 +0900

    dotenv 패키지 추가

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const authRoutes = require('./routes/authRoutes');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, OneRoom Manager!');
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
