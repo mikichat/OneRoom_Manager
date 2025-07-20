b18ea8cbeebbbdb9646abd6e787d753aa4816e03
mikichat <mikichat@gmail.com>
2025-07-20 21:51:56 +0900

    Fix: Add Contract association to Tenant model

--- backend/models/tenant.js
+++ backend/models/tenant.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Tenant.hasMany(models.Contract, {
+        foreignKey: 'tenant_id',
+        as: 'contracts'
+      });
       // define association here
     }
   }
